6936e185cae29a9e38e0093468b08a8beb2df5d1
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-03-29 18:29:10 +0530

    add tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-import React, {  useState } from 'react'
+import React from 'react'
+
 
 const App = () => {
-  const [a, setA] = useState(10)
-  
-  const increment = () => {
-    setA(a + 1)
-  }
-  const Decrement = () => {
-    setA(a - 1)
-  }
-
   return (
     <div>
-      <h2>Number is {a}</h2>
-       <button onClick={increment}>Increment</button>
-       <button onClick={Decrement}>Decrement</button>
+       <h1 className="text-8xl font-bold underline">
+    Hello World! 
+  </h1>
     </div>
   )
 }
