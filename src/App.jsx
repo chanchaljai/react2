c3fd29f21dfd1e6b52672b771414cee443e31f62
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-03-30 07:11:34 +0530

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
 
+
+
 const App = () => {
   return (
-    <div>
-       <h1 className="text-8xl font-bold underline">
-    Hello World! 
-  </h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <form className="bg-white p-6 rounded shadow-md space-y-4 w-full max-w-md">
+        <input
+          className="w-full px-4 py-3 border border-gray-300 rounded"
+          type="text"
+          placeholder="Enter Your Name"
+        />
+        <button
+          className="w-full px-4 py-3 bg-gray-400 text-white rounded hover:bg-gray-500 transition-colors"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   )
 }
