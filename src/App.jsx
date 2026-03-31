b2ba58a776b25d8bdd9d56786996f752c803aae0
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-03-31 20:37:28 +0530

    two way binding

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [name, setName] = useState("")
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log(name)
+    setName("")
 
-   const submitHandler = (e)=> {
-         e.preventDefault()
-    }
+
+  }
   return (
-    <div className="flex justify-center items-center h-screen">
-      <form className="flex flex-col gap-4 border p-6 rounded-lg" onSubmit={submitHandler}>
-        
-        <input 
-          className="px-4 py-2 border border-gray-400 rounded"
-          type="text" 
-          placeholder="Enter Your Name" 
-        />
-
-        <button className="px-4 py-2 bg-green-400 rounded text-white">
-          Submit
-        </button>
-
+    <div className='flex justify-center items-center h-screen'>
+      <form className='flex flex-col gap-2' onSubmit={submitHandler}>
+        <input className='bg-gray-200 rounded border border-gray-400 p-2' type="text" placeholder='Enter Your Name'
+          value={name} onChange={(e)=>{setName(e.target.value)}}
+           
+          />
+        <button className='bg-green-500 rounded text-white p-2' type="submit">Submit</button>
       </form>
     </div>
   )
