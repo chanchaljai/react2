68b42f76f018556268555436cb97c6c4c28a1c59
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-02 11:49:38 +0530

    componenets

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
-import React from 'react'
-import Header from './components/Header.jsx'
+import React from "react";
+import Header from "./components/Header.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
   return (
-    <div className='bg-gray-900 min-h-screen text-white'>
-      
-     <Header />
-     <Header></Header>
-     <Header/>
-      <nav className='flex items-center justify-between py-2 px-8 bg-emerald-500'>
-        <h2 className='text-2xl'>Chanchal Jaiswal</h2>
-        <div className='flex gap-8 items-center'>
-          <a className='text-xl' href="#">About</a>
-          <a className='text-xl' href="#">Servises</a>
-          <a className='text-xl' href="#">Contact</a>
-          <a className='text-xl' href="#">Home</a>
-        </div>
-      </nav>
+    <div className="bg-gray-900 min-h-screen text-white">
+      <Header />
+      <Navbar />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <h3 className='text-2xl text-center bg-pink-500 w-full py-2 fixed bottom-0'>This is Footer</h3>
+    </div>
+  )
+}
+
+export default Footer
