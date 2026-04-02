0667328f7353b27047020cff53c5520c2b580035
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-02 21:28:13 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Card from './components/Card'
 
 const App = () => {
-  const num = 10
   return (
     <div>
-      <Card a={num} />
+      <div className='p-10'>
+        <Card user={{name: 'chanchal', surname: 'jaiswal', city: 'motihari', age: 30}}  />
+      </div>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const Card = (props) => {
-    console.log(props.a)
+    console.log(props)
   return (
-    <div className='text-white text-3xl'>
-        <h1>Username is {props.a} </h1>
-        </div>
+    <div className='bg-white inline-block p-4 text-center rounded'>
+        <h1>{props.user.name} {props.user.surname}</h1>
+        <h2>{props.user.city}, {props.user.age}</h2>
+        <button>Add Friend</button>
+    </div>
   )
 }
 
