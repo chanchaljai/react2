e148bc151646d84bd8728bc60742dbccecbd9c22
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-03 11:14:45 +0530

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,55 @@
-import React from 'react'
-import Card from './components/Card'
+import React from "react";
+import Card from "./components/Card";
 
 const App = () => {
-  return (
-    <div>
-      <div className='p-10'>
-        <Card user={{name: 'chanchal', surname: 'jaiswal', city: 'motihari', age: 30}}  />
-      </div>
-    </div>
-  )
-}
+  const users = [
+  {
+    "name": "Rahul Sharma",
+    "city": "Delhi",
+    "age": 25,
+    "profession": "Software Developer",
+    "profilePic": "https://randomuser.me/api/portraits/men/1.jpg"
+  },
+  {
+    "name": "Priya Verma",
+    "city": "Mumbai",
+    "age": 28,
+    "profession": "UI/UX Designer",
+    "profilePic": "https://randomuser.me/api/portraits/women/2.jpg"
+  },
+  {
+    "name": "Amit Kumar",
+    "city": "Bangalore",
+    "age": 30,
+    "profession": "Data Analyst",
+    "profilePic": "https://randomuser.me/api/portraits/men/3.jpg"
+  },
+  {
+    "name": "Sneha Gupta",
+    "city": "Kolkata",
+    "age": 24,
+    "profession": "Frontend Developer",
+    "profilePic": "https://randomuser.me/api/portraits/women/4.jpg"
+  },
+  {
+    "name": "Vikash Singh",
+    "city": "Patna",
+    "age": 27,
+    "profession": "Backend Developer",
+    "profilePic": "https://randomuser.me/api/portraits/men/5.jpg"
+  }
+]
 
-export default App
+
+  return (
+    <>
+      <div className="p-4 flex gap-4">
+       {users.map(function(elem,idx){
+          return <Card key={idx} name={elem.name} city={elem.city} age={elem.age} profession={elem.profession} profilePic={elem.profilePic}/>
+       })}
+      </div>
+    </>
+  );
+};
+
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
 const Card = (props) => {
-    console.log(props)
+ 
+  
   return (
-    <div className='bg-white inline-block p-4 text-center rounded'>
-        <h1>{props.user.name} {props.user.surname}</h1>
-        <h2>{props.user.city}, {props.user.age}</h2>
-        <button>Add Friend</button>
+    <div>
+        <div className='bg-white text-black inline-block text-center px-3 py-2'>
+          <img className='h-32 w-32 rounded-full m-auto mb-3' src={props.profilePic} alt="" />
+          <h1 className='text-2xl font-semibold mb-1'>{props.name}</h1>
+          <h3 className='text-blue-700 test-xs'>{props.profession}</h3>
+          <h2>{props.city}, {props.age}</h2>
+          <button className='bg-blue-700 text-white rounded p-1 text-sm hover:cursor-pointer mt-3'>Add Friend</button>
+
+        </div>
     </div>
   )
 }
