041275b0547be7c6c92f4a159129f681cdd9f26e
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-03 12:52:01 +0530

    Integrating API axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,24 @@
+import axios from "axios";
 import React from "react";
-import Card from "./components/Card";
-import Ex from "./components/Ex";
 
 const App = () => {
-  const users = [
-  {
-    "name": "Rahul Sharma",
-    "city": "Delhi",
-    "age": 25,
-    "profession": "Software Developer",
-    "profilePic": "https://randomuser.me/api/portraits/men/1.jpg"
-  },
-  {
-    "name": "Priya Verma",
-    "city": "Mumbai",
-    "age": 28,
-    "profession": "UI/UX Designer",
-    "profilePic": "https://randomuser.me/api/portraits/women/2.jpg"
-  },
-  {
-    "name": "Amit Kumar",
-    "city": "Bangalore",
-    "age": 30,
-    "profession": "Data Analyst",
-    "profilePic": "https://randomuser.me/api/portraits/men/3.jpg"
-  },
-  {
-    "name": "Sneha Gupta",
-    "city": "Kolkata",
-    "age": 24,
-    "profession": "Frontend Developer",
-    "profilePic": "https://randomuser.me/api/portraits/women/4.jpg"
-  },
-  {
-    "name": "Vikash Singh",
-    "city": "Patna",
-    "age": 27,
-    "profession": "Backend Developer",
-    "profilePic": "https://randomuser.me/api/portraits/men/5.jpg"
+
+  const getData = async() => {
+   const response = await axios.get('https://picsum.photos/v2/list')
+   console.log(response)
   }
-]
-
-
   return (
-    <>
-      <div className="p-4 flex gap-4">
-       {users.map(function(elem,idx){
-          return <Card key={idx} name={elem.name} city={elem.city} age={elem.age} profession={elem.profession} profilePic={elem.profilePic}/>
-       })}
+    <div className="p-10">
+      <button
+        onClick={getData}
+        className="bg-green-600 text-white rounded text-3xl active:scale-90 hover:cursor-pointer hover:scale-105"
+      >
+        Get Data
+      </button>
+      <div className="p-5 mt-5 bg-red-950 text-white">
+        Hello its data save Point
       </div>
-      <Ex name="Rahul" />
-       
-    </>
+    </div>
   );
 };
 
